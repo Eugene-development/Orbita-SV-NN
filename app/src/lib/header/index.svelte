e874be86067b4579c89e5215c6c9c076bab7a3b8
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-05 15:36:53 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -6,13 +6,13 @@
     <!-- This example requires Tailwind CSS v2.0+ -->
     <div class="relative bg-gradient-to-tr from-indigo-500 via-indigo-600 to-indigo-500">
         <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-            <div class="pr-16 sm:text-center sm:px-16">
-                <p class="font-medium text-white">
-                    <span class="md:hidden text-lg">8 (800) 555-8-111</span>
-                    <span class="hidden md:inline text-lg">8 (800) 555-8-111</span>
-                    <span class="block sm:ml-4 sm:inline-block">info@orbita-stroy.com</span>
-                    <span class="block sm:ml-4 sm:inline-block">Пн-Пт 8:00-17:30; Сб 9:00-16:00</span>
-                </p>
+            <div class="pr-16  sm:text-center sm:px-16">
+                <div class="font-medium text-white text-lg">
+                    <span class="md:hidden">тел: 8 (800) 555-8-111</span>
+                    <span class="hidden md:inline">тел: 8 (800) 555-8-111</span>
+                    <span class="block sm:ml-4 sm:inline-block">почта: info@orbita-stroy.com</span>
+                    <span class="block sm:ml-4 sm:inline-block">режим: Пн-Пт 8:00-17:30; Сб 9:00-16:00</span>
+                </div>
             </div>
         </div>
     </div>
@@ -356,118 +356,61 @@
                         </div>
                     </nav>
                     <div class="flex items-center md:ml-12">
-                        <a href="#" class="text-base font-medium text-gray-500 hover:text-gray-900"> Sign in </a>
-                        <a href="#" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
+                        <a href="#" class="ml-8 inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Корзина (0) </a>
                     </div>
                 </div>
             </div>
         </div>
 
-        <!--
-          Mobile menu, show/hide based on mobile menu state.
+        <MobileMenu/>
 
-          Entering: "duration-200 ease-out"
-            From: "opacity-0 scale-95"
-            To: "opacity-100 scale-100"
-          Leaving: "duration-100 ease-in"
-            From: "opacity-100 scale-100"
-            To: "opacity-0 scale-95"
-        -->
-        <div class="absolute z-30 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
-            <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
-                <div class="pt-5 pb-6 px-5 sm:pb-8">
-                    <div class="flex items-center justify-between">
-                        <div>
-                            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
-                        </div>
-                        <div class="-mr-2">
-                            <button type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-                                <span class="sr-only">Close menu</span>
-                                <!-- Heroicon name: outline/x -->
-                                <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                    <div class="mt-6 sm:mt-8">
-                        <nav>
-                            <div class="grid gap-7 sm:grid-cols-2 sm:gap-y-8 sm:gap-x-4">
-                                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                    <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                        <!-- Heroicon name: outline/chart-bar -->
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
-                                        </svg>
-                                    </div>
-                                    <div class="ml-4 text-base font-medium text-gray-900">Analytics</div>
-                                </a>
-
-                                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                    <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                        <!-- Heroicon name: outline/cursor-click -->
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
-                                        </svg>
-                                    </div>
-                                    <div class="ml-4 text-base font-medium text-gray-900">Engagement</div>
-                                </a>
-
-                                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                    <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                        <!-- Heroicon name: outline/shield-check -->
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                                        </svg>
-                                    </div>
-                                    <div class="ml-4 text-base font-medium text-gray-900">Security</div>
-                                </a>
-
-                                <a href="#" class="-m-3 flex items-center p-3 rounded-lg hover:bg-gray-50">
-                                    <div class="flex-shrink-0 flex items-center justify-center h-10 w-10 rounded-md bg-indigo-500 text-white sm:h-12 sm:w-12">
-                                        <!-- Heroicon name: outline/view-grid -->
-                                        <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                                        </svg>
-                                    </div>
-                                    <div class="ml-4 text-base font-medium text-gray-900">Integrations</div>
-                                </a>
-                            </div>
-                            <div class="mt-8 text-base">
-                                <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> View all products <span aria-hidden="true">&rarr;</span></a>
-                            </div>
-                        </nav>
-                    </div>
-                </div>
-                <div class="py-6 px-5">
-                    <div class="grid grid-cols-2 gap-4">
-                        <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Pricing </a>
-
-                        <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Docs </a>
-
-                        <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Company </a>
-
-                        <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Resources </a>
-
-                        <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Blog </a>
-
-                        <a href="#" class="rounded-md text-base font-medium text-gray-900 hover:text-gray-700"> Contact Sales </a>
-                    </div>
-                    <div class="mt-6">
-                        <a href="#" class="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"> Sign up </a>
-                        <p class="mt-6 text-center text-base font-medium text-gray-500">
-                            Existing customer?
-                            <a href="#" class="text-indigo-600 hover:text-indigo-500"> Sign in </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 
 
+    <div class="bg-gray-50 pt-6 sm:pt-8">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div class="max-w-4xl mx-auto text-center">
+                <h2 class="text-3xl font-extrabold text-gray-800 sm:text-4xl">
+                    Интернет-магазин строительных и отделочных материалов "Орбита-строй"
+                </h2>
+                <p class="mt-3 text-xl text-red-900 sm:mt-4">
+                    При заказе через каталог стройматериалов на сайте для вас скидка 5%
+                </p>
+            </div>
+        </div>
+        <div class="mt-6 pb-4 bg-white sm:pb-8">
+            <div class="relative">
+                <div class="absolute inset-0 h-1/2 bg-gray-50 border-b border-gray-200"></div>
+                <div class="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class="w-full mx-auto">
+                        <dl class="rounded-lg bg-white shadow-lg lg:grid lg:grid-cols-7 ">
+<!--                            <NuxtLink v-for="(head, idx) of allHeads" :key="allHeads.id"-->
+<!--                                      :to="'/rubric/' + head.slug"-->
+<!--                                      class="flex flex-col bg-red-900 hover:bg-red-800 border-b border-red-200 p-4 text-center sm:border-0 sm:border-r sm:border-l">-->
+<!--                                <dt class="order-2 mt-1 text-base leading-6 font-medium text-gray-50">-->
+<!--                                    {{ head.name }}-->
+<!--                                </dt>-->
+<!--                                &lt;!&ndash;                  <dd class="order-1 text-5xl font-extrabold text-indigo-600">&ndash;&gt;-->
+<!--                                &lt;!&ndash;                    86&ndash;&gt;-->
+<!--                                &lt;!&ndash;                  </dd>&ndash;&gt;-->
+<!--                            </NuxtLink>-->
+<!--                            <NuxtLink-->
+<!--                                    to="/rubric/okna"-->
+<!--                                    class="flex flex-col bg-red-900 hover:bg-red-800 border-b border-red-200 p-4 text-center sm:border-0 sm:border-r sm:border-l">-->
+<!--                                <dt class="order-2 mt-1 text-base leading-6 font-medium text-gray-50">-->
+<!--                                    Окна-->
+<!--                                </dt>-->
+<!--                                &lt;!&ndash;                  <dd class="order-1 text-5xl font-extrabold text-indigo-600">&ndash;&gt;-->
+<!--                                &lt;!&ndash;                    86&ndash;&gt;-->
+<!--                                &lt;!&ndash;                  </dd>&ndash;&gt;-->
+<!--                            </NuxtLink>-->
+                        </dl>
+                    </div>
+                </div>
+            </div>
 
+        </div>
+    </div>
 
-    <MobileMenu/>
 </header>
 
