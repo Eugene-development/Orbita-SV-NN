6564b19dca0c1d83334dc7edc66a8638cbfc3609
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-06 19:45:09 +0300

    git init

--- app/src/routes/information/contact/index.svelte
+++ app/src/routes/information/contact/index.svelte
@@ -87,19 +87,19 @@
                         <div>
                             <label for="first_name" class="block text-sm font-medium text-gray-900">Имя</label>
                             <div class="mt-1">
-                                <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                                <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md border border-gray-300">
                             </div>
                         </div>
                         <div>
                             <label for="last_name" class="block text-sm font-medium text-gray-900">Должность</label>
                             <div class="mt-1">
-                                <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                                <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md border border-gray-300">
                             </div>
                         </div>
                         <div>
                             <label for="email" class="block text-sm font-medium text-gray-900">Почта</label>
                             <div class="mt-1">
-                                <input id="email" name="email" type="email" autocomplete="email" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                                <input id="email" name="email" type="email" autocomplete="email" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md border border-gray-300">
                             </div>
                         </div>
                         <div>
@@ -107,7 +107,7 @@
                                 <label for="phone" class="block text-sm font-medium text-gray-900">Телефон</label>
                             </div>
                             <div class="mt-1">
-                                <input type="text" name="phone" id="phone" autocomplete="tel" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md" aria-describedby="phone-optional">
+                                <input type="text" name="phone" id="phone" autocomplete="tel" class="py-3 px-4 block w-full shadow-sm text-gray-900 focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md border border-gray-300" aria-describedby="phone-optional">
                             </div>
                         </div>
                         <div class="sm:col-span-2">
@@ -129,7 +129,7 @@
                                         <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                     </svg>
                                     <div class="flex text-sm text-gray-600">
-                                        <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                        <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-red-800 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
                                             <span>Загрузите файл</span>
                                             <input id="file-upload" name="file-upload" type="file" class="sr-only">
                                         </label>
@@ -148,7 +148,7 @@
 
 
                         <div class="sm:col-span-2 sm:flex sm:justify-end">
-                            <button type="submit" class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto">
+                            <button type="submit" class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-red-800 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto">
                                 Отправить
                             </button>
                         </div>
