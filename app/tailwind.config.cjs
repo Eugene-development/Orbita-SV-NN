2f47c9c8174eff41e8c7487f4ae3f0235ace9999
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-06 21:05:25 +0300

    git init

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -3,5 +3,10 @@ module.exports = {
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ]
 };
