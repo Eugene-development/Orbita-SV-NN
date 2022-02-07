81b048795d097598f95fdca877801ca21478d63b
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-07 22:15:09 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -1,9 +1,17 @@
 <script>
     import MobileMenu from "./mobile/index.svelte"
-
     import {useHead} from "../../use/content/header";
-
     const {head} = useHead;
+
+
+    import {useVisible} from "../../use/visible";
+    import {visibleInformationMenu} from '../../stores.js';
+
+    const {invert} = useVisible;
+
+    const changeVisibleInformationMenu = () => visibleInformationMenu.update(invert)
+    let visibleFormPhone;
+    visibleInformationMenu.subscribe(value => visibleFormPhone = value);
 
 </script>
 
@@ -46,17 +54,16 @@
                 <div class="hidden md:flex-1 md:flex md:items-center md:justify-between">
                     <nav class="flex space-x-10">
                         <a href="/company" class="text-base font-medium text-gray-500 hover:text-gray-900"> Компания </a>
-                        <a href="/information/action" class="text-base font-medium text-gray-500 hover:text-gray-900"> Акции </a>
                         <div>
                             <!-- Item active: "text-gray-900", Item inactive: "text-gray-500" -->
-                            <button type="button" class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" aria-expanded="false">
+                            <button on:click={changeVisibleInformationMenu} type="button" class="text-gray-500 group bg-white rounded-md inline-flex items-center text-base font-medium focus:outline-none focus:text-red-700" aria-expanded="false">
                                 <span>Информация</span>
                                 <!--
                                   Heroicon name: solid/chevron-down
 
                                   Item active: "text-gray-600", Item inactive: "text-gray-400"
                                 -->
-                                <svg class="text-red-700 ml-2 h-5 w-5 group-hover:text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                <svg class="text-red-700 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                                     <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                 </svg>
                             </button>
@@ -71,7 +78,7 @@
                                 From: "opacity-100 translate-y-0"
                                 To: "opacity-0 -translate-y-1"
                             -->
-                            {#if false}
+                            {#if visibleFormPhone}
                                 <div class="hidden md:block absolute z-10 top-full inset-x-0 transform shadow-lg">
                                     <div class="absolute inset-0 flex">
                                         <div class="bg-white w-1/2"></div>
@@ -80,7 +87,7 @@
                                     <div class="relative max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2">
                                         <nav class="grid gap-y-10 px-4 py-8 bg-white sm:grid-cols-2 sm:gap-x-8 sm:py-12 sm:px-6 lg:px-8 xl:pr-12">
                                             <div>
-                                                <h3 class="text-sm font-medium tracking-wide text-gray-500 uppercase">Company</h3>
+<!--                                                <h3 class="text-sm font-medium tracking-wide text-gray-500 uppercase">Company</h3>-->
                                                 <ul role="list" class="mt-5 space-y-6">
                                                     <li class="flow-root">
                                                         <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
@@ -88,7 +95,17 @@
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                                                             </svg>
-                                                            <span class="ml-4">About</span>
+                                                            <span class="ml-4">Акции</span>
+                                                        </a>
+                                                    </li>
+
+                                                    <li class="flow-root">
+                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                            <!-- Heroicon name: outline/information-circle -->
+                                                            <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                                                            </svg>
+                                                            <span class="ml-4">Оплата</span>
                                                         </a>
                                                     </li>
 
@@ -98,7 +115,7 @@
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
                                                             </svg>
-                                                            <span class="ml-4">Customers</span>
+                                                            <span class="ml-4">Доставка</span>
                                                         </a>
                                                     </li>
 
@@ -108,33 +125,24 @@
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                                                             </svg>
-                                                            <span class="ml-4">Press</span>
+                                                            <span class="ml-4">Грузчики</span>
                                                         </a>
                                                     </li>
 
                                                     <li class="flow-root">
                                                         <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
-                                                            <!-- Heroicon name: outline/briefcase -->
+                                                            <!-- Heroicon name: outline/newspaper -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
                                                             </svg>
-                                                            <span class="ml-4">Careers</span>
+                                                            <span class="ml-4">Документы</span>
                                                         </a>
                                                     </li>
 
-                                                    <li class="flow-root">
-                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
-                                                            <!-- Heroicon name: outline/shield-check -->
-                                                            <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
-                                                            </svg>
-                                                            <span class="ml-4">Privacy</span>
-                                                        </a>
-                                                    </li>
                                                 </ul>
                                             </div>
                                             <div>
-                                                <h3 class="text-sm font-medium tracking-wide text-gray-500 uppercase">Resources</h3>
+<!--                                                <h3 class="text-sm font-medium tracking-wide text-gray-500 uppercase">Resources</h3>-->
                                                 <ul role="list" class="mt-5 space-y-6">
                                                     <li class="flow-root">
                                                         <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
@@ -142,7 +150,7 @@
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
                                                             </svg>
-                                                            <span class="ml-4">Community</span>
+                                                            <span class="ml-4">Цены</span>
                                                         </a>
                                                     </li>
 
@@ -152,7 +160,7 @@
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
                                                             </svg>
-                                                            <span class="ml-4">Partners</span>
+                                                            <span class="ml-4">Отзывы</span>
                                                         </a>
                                                     </li>
 
@@ -162,19 +170,30 @@
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                                             </svg>
-                                                            <span class="ml-4">Guides</span>
+                                                            <span class="ml-4">Гарантии</span>
                                                         </a>
                                                     </li>
 
                                                     <li class="flow-root">
                                                         <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
-                                                            <!-- Heroicon name: outline/desktop-computer -->
+                                                            <!-- Heroicon name: outline/bookmark-alt -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
                                                             </svg>
-                                                            <span class="ml-4">Webinars</span>
+                                                            <span class="ml-4">Партнёрство</span>
                                                         </a>
                                                     </li>
+
+                                                    <li class="flow-root">
+                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                            <!-- Heroicon name: outline/bookmark-alt -->
+                                                            <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                                            </svg>
+                                                            <span class="ml-4">Сертификаты</span>
+                                                        </a>
+                                                    </li>
+
                                                 </ul>
                                             </div>
                                         </nav>
@@ -208,7 +227,7 @@
                                                 </ul>
                                             </div>
                                             <div class="mt-6 text-sm font-medium">
-                                                <a href="/blog" class="text-indigo-600 hover:text-indigo-500"> Перейти в блог <span aria-hidden="true">&rarr;</span></a>
+                                                <a href="/blog" class="text-red-700 hover:text-red-800"> Перейти в блог <span aria-hidden="true">&rarr;</span></a>
                                             </div>
                                         </div>
                                     </div>
