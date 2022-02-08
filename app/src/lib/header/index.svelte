da433f4e6d77f06ba76a1aecc93d4195e02d3b4d
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-08 17:35:04 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -90,7 +90,7 @@
 <!--                                                <h3 class="text-sm font-medium tracking-wide text-gray-500 uppercase">Company</h3>-->
                                                 <ul role="list" class="mt-5 space-y-6">
                                                     <li class="flow-root">
-                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                        <a href="/information/action" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
                                                             <!-- Heroicon name: outline/information-circle -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -100,7 +100,7 @@
                                                     </li>
 
                                                     <li class="flow-root">
-                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                        <a href="/information/payment" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
                                                             <!-- Heroicon name: outline/information-circle -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -110,7 +110,7 @@
                                                     </li>
 
                                                     <li class="flow-root">
-                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                        <a href="/information/delivery" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
                                                             <!-- Heroicon name: outline/office-building -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" />
@@ -120,7 +120,7 @@
                                                     </li>
 
                                                     <li class="flow-root">
-                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                        <a href="/information/movers" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
                                                             <!-- Heroicon name: outline/newspaper -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
@@ -155,7 +155,7 @@
                                                     </li>
 
                                                     <li class="flow-root">
-                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                        <a href="/information/testimonials" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
                                                             <!-- Heroicon name: outline/globe-alt -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
@@ -175,7 +175,7 @@
                                                     </li>
 
                                                     <li class="flow-root">
-                                                        <a href="#" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
+                                                        <a href="/information/partner" class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-50">
                                                             <!-- Heroicon name: outline/bookmark-alt -->
                                                             <svg class="flex-shrink-0 h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
--- app/src/use/content/header/index.js
+++ app/src/use/content/header/index.js
@@ -1,11 +1,61 @@
+const information = [
+    {
+        left: [
+            {
+                displayName: 'Акции',
+                href: '/information/action'
+            },
+            {
+                displayName: 'Оплата',
+                href: '/information/payment'
+            },
+            {
+                displayName: 'Доставка',
+                href: '/information/delivery'
+            },
+            {
+                displayName: 'Грузчики',
+                href: '/information/movers'
+            },
+            {
+                displayName: 'Документы',
+                href: '/information/documents'
+            },
+        ],
+        right: [
+            {
+                displayName: 'Цены',
+                href: '/information/price'
+            },
+            {
+                displayName: 'Отзывы',
+                href: '/information/testimonials'
+            },
+            {
+                displayName: 'Гарантии',
+                href: '/information/guarantees'
+            },
+            {
+                displayName: 'Партнёрство',
+                href: '/information/partner'
+            },
+            {
+                displayName: 'Сертификаты',
+                href: '/information/certificates'
+            },
+        ]
+    }
+]
+
+
 const head = [
     {
-        id:1,
+        id: 1,
         name: 'Стройматериалы',
         slug: 'stroymaterialy'
     },
     {
-        id:2,
+        id: 2,
         name: 'Изоляция',
         slug: 'izolyatsiya'
     },
