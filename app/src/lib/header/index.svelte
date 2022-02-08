cb0fd96e66b05f594e558836a658ff2f436e8074
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-08 18:30:29 +0300

    git init

--- app/src/lib/header/index.svelte
+++ app/src/lib/header/index.svelte
@@ -192,7 +192,7 @@
                         <div class="rounded-lg bg-white shadow-xl shadow-indigo-700/50 lg:grid lg:grid-cols-7 ">
                             {#each head as {slug, name}}
                             <a href='/rubric/{slug}'
-                                      class="flex flex-col bg-gradient-to-tr from-indigo-500 via-indigo-700 to-indigo-600 border-b border-gray-100 p-4 text-center sm:border-0 sm:border-r sm:border-l">
+                                      class="flex flex-col bg-gradient-to-tr hover:bg-gradient-to-bl from-indigo-500 via-indigo-700 to-indigo-600 border-b border-gray-100 p-4 text-center sm:border-0 sm:border-r sm:border-l hover:bg-red-800">
                                 <span class="order-2 mt-1 text-base leading-6 font-medium text-gray-50">
                                     { name }
                                 </span>
