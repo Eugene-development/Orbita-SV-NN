4870e0ed511e026955566faf1289b14d7061b0a2
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-16 12:22:17 +0300

    git init

--- app/src/lib/footer/index.svelte
+++ app/src/lib/footer/index.svelte
@@ -66,29 +66,27 @@
                         </div>
                     </div>
 
-                    <div class="md:grid md:grid-cols-2 md:gap-8">
+                    <div class="md:grid md:grid-cols-2 md:gap-8 ">
                         <div>
                             <h3 class="text-lg font-semibold text-gray-50 tracking-wider uppercase">
                                 Клиентам:
                             </h3>
-                            <div class="mt-4 space-y-4">
+                            <div class="mt-4 space-y-4 ">
                                 <div class="flow-root">
-                                    <a
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium "
                                             href="/information/payment">
                                         <!-- Heroicon name: information-circle -->
-                                        <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
+                                        <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50 " fill="none"
                                              stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                                             <path d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Оплата</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Оплата</span>
                                     </a>
                                 </div>
 
                                 <div class="flow-root">
-                                    <a
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium "
                                             href="/information/delivery">
                                         <!-- Heroicon name: office-building -->
                                         <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
@@ -96,13 +94,12 @@
                                             <path d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Доставка</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Доставка</span>
                                     </a>
                                 </div>
 
                                 <div class="flow-root">
-                                    <a
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium"
                                             href="/information/movers">
                                         <!-- Heroicon name: newspaper -->
                                         <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
@@ -110,13 +107,12 @@
                                             <path d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Грузчики</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Грузчики</span>
                                     </a>
                                 </div>
 
                                 <div class="flow-root">
-                                    <a
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium"
                                             href="/information/contact">
                                         <!-- Heroicon name: briefcase -->
                                         <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
@@ -124,7 +120,7 @@
                                             <path d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Контакты</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Контакты</span>
                                     </a>
                                 </div>
 
@@ -137,8 +133,7 @@
 
                             <div class="mt-4 space-y-4">
                                 <div class="flow-root">
-                                    <a
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium"
                                             href="/information/discount">
                                         <!-- Heroicon name: user-group -->
                                         <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
@@ -146,13 +141,12 @@
                                             <path d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Скидки</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Скидки</span>
                                     </a>
                                 </div>
 
                                 <div class="flow-root">
-                                    <a
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium"
                                             href="/information/testimonials">
                                         <!-- Heroicon name: globe-alt -->
                                         <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
@@ -160,13 +154,12 @@
                                             <path d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Отзывы</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Отзывы</span>
                                     </a>
                                 </div>
 
                                 <div class="flow-root">
-                                    <a
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium"
                                             href="/information/action">
                                         <!-- Heroicon name: bookmark-alt -->
                                         <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
@@ -174,14 +167,12 @@
                                             <path d="M16 4v12l-4-2-4 2V4M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Акции</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Акции</span>
                                     </a>
                                 </div>
 
                                 <div class="flow-root">
-                                    <a
-
-                                            class="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900"
+                                    <a class="-m-3 p-3 flex items-center rounded-md text-base font-medium"
                                             href="/information/partner">
                                         <!-- Heroicon name: desktop-computer -->
                                         <svg aria-hidden="true" class="flex-shrink-0 h-6 w-6 text-gray-50" fill="none"
@@ -189,7 +180,7 @@
                                             <path d="M9.75 17L9 20l-1 1h8l-1-1-.75-3M3 13h18M5 17h14a2 2 0 002-2V5a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" stroke-linecap="round" stroke-linejoin="round"
                                                   stroke-width="2"/>
                                         </svg>
-                                        <span class="ml-4 text-gray-50">Сотрудничество</span>
+                                        <span class="ml-4 text-gray-50 hover:text-indigo-500">Сотрудничество</span>
                                     </a>
                                 </div>
                             </div>
