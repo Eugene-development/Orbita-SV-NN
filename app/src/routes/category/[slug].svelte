51551a5e1bfefa82b6766c94e93356c31115f53b
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-19 20:30:42 +0300

    git init

--- app/src/routes/category/[slug].svelte
+++ app/src/routes/category/[slug].svelte
@@ -12,6 +12,8 @@
         return {
             props: {
                 nameCategory,
+                category,
+                text
             }
         }
     }
@@ -19,6 +21,8 @@
 
 <script>
     export let nameCategory
+    export let category
+    export let text
 </script>
 
 
@@ -31,6 +35,134 @@
         </div>
     </div>
 
+    <div class="mx-4 flex  ">
+        <div class="flex w-1/5 max-w-sm flex-col flex-grow border-r border-gray-200 pt-8 pb-4 overflow-y-auto           hidden xl:block">
+            <div class="mb-6 flex items-center flex-shrink-0 px-4">
+                <!--          <svg class="text-gray-500 mr-3 h-8 w-8" fill="none" stroke="currentColor" viewBox="0 0 24 24"-->
+                <!--               xmlns="http://www.w3.org/2000/svg">-->
+                <!--            <path-->
+                <!--              d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"-->
+                <!--              stroke-linecap="round" stroke-linejoin="round"-->
+                <!--              stroke-width="2"/>-->
+                <!--          </svg>-->
+                <div class="text-gray-900 text-2xl">
+                    <strong>{ nameCategory }</strong>
+                </div>
+            </div>
+
+            <hr>
+            <div class="mt-5 flex-grow flex flex-col">
+                <nav aria-label="Sidebar" class="flex-1 px-2 space-y-1">
+                    <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
+
+                    {#each category as {id, slug, name}}
+                    <a href="'/products/' + category.slug"
+                              class="text-gray-600 hover:bg-gray-100 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                        <!-- Heroicon name: outline/folder -->
+                        <!--                <svg aria-hidden="true" class="text-gray-400 group-hover:text-gray-500 mr-3 h-6 w-6"-->
+                        <!--                     fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">-->
+                        <!--                  <path d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"-->
+                        <!--                        stroke-linecap="round"-->
+                        <!--                        stroke-linejoin="round"-->
+                        <!--                        stroke-width="2"/>-->
+                        <!--                </svg>-->
+
+                        { name }
+
+                        <span class="bg-gray-100 group-hover:bg-gray-200 ml-auto inline-block py-0.5 px-3 text-xs font-medium rounded-full">
+                          <!--{{ category.product_count }}-->
+                        </span>
+                    </a>
+                    {/each}
+                </nav>
+            </div>
+        </div>
+
+        <!-- This example requires Tailwind CSS v2.0+ -->
+        <div class="relative pt-4 pb-4 px-4 sm:px-6 lg:pt-4 lg:pb-8 lg:px-8">
+
+
+
+            <!-- This example requires Tailwind CSS v2.0+ -->
+<!--            <div class="py-1 relative shadow-lg rounded-lg border-2">-->
+<!--                &lt;!&ndash;          <div class="py-1 relative bg-gradient-to-b from-gray-50 via-gray-800 to-gray-50 shadow-lg rounded-lg">&ndash;&gt;-->
+<!--                <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">-->
+<!--                    <div v-if="rubric.text" class="text-center px-8">-->
+<!--                        <p class="font-medium text-gray-800 text-xl">-->
+<!--                            <span v-html="rubric.text.descriptionText"></span>-->
+<!--                        </p>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+
+
+<!--            <div  class="relative max-w-7xl mx-auto">-->
+<!--                &lt;!&ndash;          <div class="text-center">&ndash;&gt;-->
+<!--                &lt;!&ndash;            <h2 class="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">&ndash;&gt;-->
+<!--                &lt;!&ndash;              {{ rubric.name }}&ndash;&gt;-->
+<!--                &lt;!&ndash;            </h2>&ndash;&gt;-->
+<!--                &lt;!&ndash;            <p class="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">&ndash;&gt;-->
+<!--                &lt;!&ndash;              Наша компания предлагает стройматериалы по самым низким ценам&ndash;&gt;-->
+<!--                &lt;!&ndash;            </p>&ndash;&gt;-->
+<!--                &lt;!&ndash;          </div>&ndash;&gt;-->
+<!--                &lt;!&ndash;          <hr class="mt-8">&ndash;&gt;-->
+<!--                <div class="mt-8 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">-->
+<!--                    <div v-for="(category, idx) of rubric.category" :key="category.id"-->
+<!--                         class=" flex flex-col rounded-lg shadow-lg overflow-hidden border-2">-->
+<!--                        &lt;!&ndash;            <div class="flex-shrink-0">&ndash;&gt;-->
+<!--                        &lt;!&ndash;              <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixqx=IrF17Golbw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80" alt="">&ndash;&gt;-->
+<!--                        &lt;!&ndash;            </div>&ndash;&gt;-->
+<!--                        <div class="flex-1 bg-gray-50 p-6 flex flex-col justify-between">-->
+<!--                            <div class="flex-1">-->
+<!--                                <NuxtLink-->
+<!--                                        :to="'/products/' + category.slug">-->
+<!--                                    <p class="text-xl font-semibold text-gray-900">-->
+<!--                                        {{ category.name }}-->
+<!--                                    </p>-->
+<!--                                    <p v-if="category.text"-->
+<!--                                       class="mt-3 text-base text-gray-500">-->
+<!--                                        {{ category.text.descriptionText }}-->
+<!--                                    </p>-->
+<!--                                    <p class="mt-2 text-base font-medium text-red-800 place-items-end">-->
+<!--                                        Выбрать &rarr;-->
+<!--                                    </p>-->
+<!--                                </NuxtLink>-->
+<!--                            </div>-->
+<!--                            &lt;!&ndash;              <div class="mt-6 flex items-center">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                <div class="flex-shrink-0">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  <a href="#">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    <span class="sr-only">Roel Aufderehar</span>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixqx=IrF17Golbw&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  </a>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                </div>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                <div class="ml-3">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  <p class="text-sm font-medium text-gray-900">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    <a href="#" class="hover:underline">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                      Roel Aufderehar&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    </a>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  </p>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  <div class="flex space-x-1 text-sm text-gray-500">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    <time datetime="2020-03-16">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                      Mar 16, 2020&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    </time>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    <span aria-hidden="true">&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  &middot;&ndash;&gt;-->
+<!--                            &lt;!&ndash;                </span>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                    <span>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  6 min read&ndash;&gt;-->
+<!--                            &lt;!&ndash;                </span>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                  </div>&ndash;&gt;-->
+<!--                            &lt;!&ndash;                </div>&ndash;&gt;-->
+<!--                            &lt;!&ndash;              </div>&ndash;&gt;-->
+<!--                        </div>-->
+<!--                    </div>-->
+
+<!--                </div>-->
+<!--            </div>-->
+        </div>
+
+        <!--            {{ new Date().toLocaleString()}}-->
+    </div>
 
 
 </div>
