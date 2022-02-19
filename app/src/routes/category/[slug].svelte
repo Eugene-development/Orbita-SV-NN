e112a347cb9d8102ff5367dbec57b18e5615dea9
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-19 20:57:47 +0300

    git init

--- app/src/routes/category/[slug].svelte
+++ app/src/routes/category/[slug].svelte
@@ -55,7 +55,7 @@
                 <nav aria-label="Sidebar" class="flex-1 px-2 space-y-1">
                     <!-- Current: "bg-gray-100 text-gray-900", Default: "text-gray-600 hover:bg-gray-50 hover:text-gray-900" -->
 
-                    {#each category as {id, slug, name}}
+                    {#each category as {id, slug, name, product}}
                     <a href="'/products/' + category.slug"
                               class="text-gray-600 hover:bg-gray-100 hover:text-gray-900 group flex items-center px-2 py-2 text-sm font-medium rounded-md">
                         <!-- Heroicon name: outline/folder -->
@@ -70,7 +70,7 @@
                         { name }
 
                         <span class="bg-gray-100 group-hover:bg-gray-200 ml-auto inline-block py-0.5 px-3 text-xs font-medium rounded-full">
-                          <!--{{ category.product_count }}-->
+                          { product.length }
                         </span>
                     </a>
                     {/each}
