606800c574021ab3e57e96883676fd3a39011745
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-20 12:20:32 +0300

    git init

--- app/src/routes/products/[slug].svelte
+++ app/src/routes/products/[slug].svelte
@@ -7,11 +7,12 @@
         const data = products.products.data[0]
         console.log(data)
         const nameCategory = data.name
-        // const category = data.category
+        const product = data.product
         // const text = data.text
         return {
             props: {
                 nameCategory,
+                product
             }
         }
     }
@@ -19,6 +20,7 @@
 
 <script>
     export let nameCategory
+    export let product
 </script>
 
 <div class="bg-blueGray-50">
@@ -28,35 +30,34 @@
         </div>
     </div>
 
-<!--    <div class="p-8">-->
-<!--        &lt;!&ndash;    <p>{{$route.params.slug}}</p>&ndash;&gt;-->
-<!--        &lt;!&ndash; This example requires Tailwind CSS v2.0+ &ndash;&gt;-->
-<!--        <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">-->
-<!--            <li v-for="(product, idx) of category.product" :key="product.id"-->
-<!--                class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow-lg divide-y divide-gray-200">-->
-<!--                <NuxtLink-->
-<!--                        :to="'/product/' + product.slug">-->
-<!--                    <div class="flex-1 flex flex-col p-8">-->
+    <div class="p-8">
+        <!--    <p>{{$route.params.slug}}</p>-->
+        <!-- This example requires Tailwind CSS v2.0+ -->
+        <ul class="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+            {#each product as {name, size, unit}}
+            <li class="col-span-1 flex flex-col text-center bg-white rounded-lg shadow-lg shadow-indigo-500/50 divide-y divide-gray-200">
+                <a href="'/product/' + product.slug">
+                    <div class="flex-1 flex flex-col p-8">
 <!--                        <img-->
 <!--                                v-for="(image, idx) of product.image" :key="image.id"-->
-<!--                                :src="`${pathAWS}${image.filename}`"-->
+<!--                                src="`${pathAWS}${image.filename}`"-->
 <!--                                alt="product"-->
 <!--                                class="w-32 h-32 object-contain flex-shrink-0 mx-auto">-->
-<!--                        <h3 class="mt-6 text-gray-900 text-sm font-medium">{{ product.name }}</h3>-->
-<!--                        <dl class="mt-1 flex-grow flex flex-col justify-between">-->
-<!--                            <dt class="sr-only">Title</dt>-->
-<!--                            <hr class="mt-4">-->
-<!--                            &lt;!&ndash;                          <dd class="text-gray-500 text-sm">цена</dd>&ndash;&gt;-->
-<!--                            <dt class="sr-only">Role</dt>-->
-<!--                            <dd class="mt-3">-->
-<!--                                <span class="px-2 py-1 text-green-800 text-base font-medium bg-green-100 rounded-full">{{ product.size[0].price.price }} р/{{ product.unit }}</span>-->
-<!--                            </dd>-->
-<!--                        </dl>-->
-<!--                    </div>-->
-<!--                </NuxtLink>-->
-<!--                <div>-->
-<!--                    <div class="-mt-px flex divide-x divide-gray-200">-->
-<!--                        <div class="w-0 flex-1 flex">-->
+                        <h3 class="mt-6 text-gray-900 text-sm font-medium">{ name }</h3>
+                        <dl class="mt-1 flex-grow flex flex-col justify-between">
+                            <dt class="sr-only">Title</dt>
+                            <hr class="mt-4">
+                            <!--                          <dd class="text-gray-500 text-sm">цена</dd>-->
+                            <dt class="sr-only">Product</dt>
+                            <dd class="mt-3">
+                                <span class="px-2 py-1 text-indigo-800 text-base font-medium bg-indigo-100 rounded-full">{ size[0].price.price } р/{ unit }</span>
+                            </dd>
+                        </dl>
+                    </div>
+                </a>
+                <div>
+                    <div class="-mt-px flex divide-x divide-gray-200">
+                        <div class="w-0 flex-1 flex">
 <!--                            <button-->
 <!--                                    v-if="!productsInCart.some(arrVal => product.id === arrVal)"-->
 <!--                                    class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"-->
@@ -81,28 +82,27 @@
 <!--                                <span class="ml-3">В корзине</span>-->
 <!--                            </button>-->
 
-<!--                        </div>-->
+                        </div>
+                        <div  class="-ml-px w-0 flex-1 flex">
 <!--                        <div v-if="productsInCart.some(arrVal => product.id === arrVal)" class="-ml-px w-0 flex-1 flex">-->
-<!--                            <NuxtLink-->
-<!--                                    :to="'/product/' + product.id"-->
-<!--                                    class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border  border-transparent rounded-bl-lg hover:text-gray-500">-->
-<!--                                &lt;!&ndash; Heroicon name: solid/mail &ndash;&gt;-->
-<!--                                <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"-->
-<!--                                     xmlns="http://www.w3.org/2000/svg">-->
-<!--                                    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" stroke-linecap="round" stroke-linejoin="round"-->
-<!--                                          stroke-width="2"/>-->
-<!--                                    <path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" stroke-linecap="round" stroke-linejoin="round"-->
-<!--                                          stroke-width="2"/>-->
-<!--                                </svg>-->
-<!--                                <span class="ml-3">-->
-<!--                     Подробнее-->
-<!--                </span>-->
-<!--                            </NuxtLink>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--            </li>-->
-<!--        </ul>-->
+                            <a href="'/product/' + product.id"
+                                    class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border  border-transparent rounded-bl-lg hover:text-gray-500">
+                                <!-- Heroicon name: solid/mail -->
+                                <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                     xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2"/>
+                                    <path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2"/>
+                                </svg>
+                                <span class="ml-3">Подробнее</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </li>
+            {/each}
+        </ul>
 
-<!--    </div>-->
+    </div>
 </div>
