fab471ec993759471ade5256fb059b0ac2260f6d
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-24 21:57:51 +0300

    git init

--- app/src/routes/products/[slug].svelte
+++ app/src/routes/products/[slug].svelte
@@ -57,20 +57,16 @@
                 <div>
                     <div class="-mt-px flex divide-x divide-gray-200">
                         <div class="w-0 flex-1 flex">
-<!--                            <button-->
-<!--                                    v-if="!productsInCart.some(arrVal => product.id === arrVal)"-->
-<!--                                    class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"-->
-<!--                                    @click.prevent.once="sendToCart (product.id)">-->
-<!--                                &lt;!&ndash; Heroicon name: solid/phone &ndash;&gt;-->
-<!--                                <svg-->
-<!--                                        class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"-->
-<!--                                        xmlns="http://www.w3.org/2000/svg">-->
-<!--                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" stroke-linecap="round" stroke-linejoin="round"-->
-<!--                                          stroke-width="2"/>-->
-<!--                                </svg>-->
-<!--                                <span class="ml-3">В корзину</span>-->
-<!--                            </button>-->
-<!--                            <button v-else-->
+                            <button
+                                    class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                                <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" stroke-linecap="round" stroke-linejoin="round"
+                                          stroke-width="2"/>
+                                </svg>
+                                <span class="ml-3">В корзину</span>
+                            </button>
+<!--                            <button -->
 <!--                                    class="focus:outline-none relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-50 font-medium border border-transparent rounded-bl-lg hover:text-gray-100 bg-indigo-900">-->
 <!--                                &lt;!&ndash; Heroicon name: solid/phone &ndash;&gt;-->
 <!--                                <svg class="w-5 h-5 text-gray-50" fill="none" stroke="currentColor" viewBox="0 0 24 24"-->
@@ -80,7 +76,6 @@
 <!--                                </svg>-->
 <!--                                <span class="ml-3">В корзине</span>-->
 <!--                            </button>-->
-
                         </div>
                         <div  class="-ml-px w-0 flex-1 flex">
 <!--                        <div v-if="productsInCart.some(arrVal => product.id === arrVal)" class="-ml-px w-0 flex-1 flex">-->
