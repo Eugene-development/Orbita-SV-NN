761559ed325d23bd0de402c90add0b6438994afa
indexpro24@gmail.com <indexpro24@gmail.com>
2022-02-27 23:50:52 +0300

    git init

--- app/src/routes/products/[slug].svelte
+++ app/src/routes/products/[slug].svelte
@@ -28,11 +28,25 @@
 </script>
 
 <script>
+    import { concat } from 'lodash';
     export let title
     export let description
     export let nameCategory
     export let products
     export let pathAWS
+
+
+    const sendToCart = (id) => {
+        if (localStorage.getItem('inCart') === null) {
+            localStorage.setItem('inCart', JSON.stringify(id));
+        } else {
+            const itemsCart = JSON.parse(localStorage.getItem('inCart'));
+            const newItemsCart = concat(itemsCart, id)
+            localStorage.setItem('inCart', JSON.stringify(newItemsCart));
+            const productsInCart = JSON.parse(localStorage.getItem('inCart'));
+        }
+    }
+
 </script>
 
 
@@ -72,36 +86,30 @@
                 <div>
                     <div class="-mt-px flex divide-x divide-gray-200">
                         <div class="w-0 flex-1 flex">
+                            {#if (true)}
                             <button
-                                    class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
-                                <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                        xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"/>
+                              on:click|preventDefault|once={sendToCart(id)}
+                              class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                                <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
                                 </svg>
                                 <span class="ml-3">В корзину</span>
                             </button>
-<!--                            <button -->
-<!--                                    class="focus:outline-none relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-50 font-medium border border-transparent rounded-bl-lg hover:text-gray-100 bg-indigo-900">-->
-<!--                                &lt;!&ndash; Heroicon name: solid/phone &ndash;&gt;-->
-<!--                                <svg class="w-5 h-5 text-gray-50" fill="none" stroke="currentColor" viewBox="0 0 24 24"-->
-<!--                                     xmlns="http://www.w3.org/2000/svg">-->
-<!--                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" stroke-linecap="round" stroke-linejoin="round"-->
-<!--                                          stroke-width="2"/>-->
-<!--                                </svg>-->
-<!--                                <span class="ml-3">В корзине</span>-->
-<!--                            </button>-->
+                            {:else }
+                            <button class="focus:outline-none relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-50 font-medium border border-transparent rounded-bl-lg hover:text-gray-100 bg-indigo-900">
+                                <svg class="w-5 h-5 text-gray-50" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                </svg>
+                                <span class="ml-3">В корзине</span>
+                            </button>
+                            {/if}
                         </div>
                         <div  class="-ml-px w-0 flex-1 flex">
 <!--                        <div v-if="productsInCart.some(arrVal => product.id === arrVal)" class="-ml-px w-0 flex-1 flex">-->
                             <a sveltekit:prefetch sveltekit:noscroll href='/product/{slug}/?id={id}' class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border  border-transparent rounded-bl-lg hover:text-gray-500">
-                                <!-- Heroicon name: solid/mail -->
-                                <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"/>
-                                    <path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" stroke-linecap="round" stroke-linejoin="round"
-                                          stroke-width="2"/>
+                                <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
+                                    <path d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/>
                                 </svg>
                                 <span class="ml-3">Подробнее</span>
                             </a>
