04afa1122d2af173df3c94068dbf7edb100e77eb
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-10 22:18:36 +0300

    git init

--- app/src/routes/shop/cart/index.svelte
+++ app/src/routes/shop/cart/index.svelte
@@ -6,7 +6,7 @@
   const l = console.log
 
   let productsInCart = [];
-  let totalSum = 0;
+
   onMount(async () => {
     const domain = import.meta.env.VITE_API_CART;
     const dataS = browser && localStorage.getItem("dataS");
@@ -18,10 +18,12 @@
     const res = await axios(url, { headers });
     productsInCart = res.data;
 
-l(productsInCart)
+    l(productsInCart)
 
 
   });
+
+
 
   // const total = productsInCart.reduce((sum, product) => {
   //   let price = 0;
@@ -29,6 +31,7 @@ l(productsInCart)
   //   return sum + price * product.quantity;
   // }, 0);
 
+  // let totalSum = 0;
   // totalSum = (total - total * 0.05).toFixed(2);
 
 
@@ -127,11 +130,11 @@ l(productsInCart)
                           bind:value={quantity}
                         >
                       </div>
-<!--                      <button type="button" class="" on:click={handleClick(id)}>-->
-<!--                        <svg  xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">-->
-<!--                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />-->
-<!--                        </svg>-->
-<!--                      </button>-->
+                      <button type="button" class="" on:click={handleClick(id)}>
+                        <svg  xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                        </svg>
+                      </button>
 
 <!--                      <button type="button">-->
 <!--                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-800" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">-->
