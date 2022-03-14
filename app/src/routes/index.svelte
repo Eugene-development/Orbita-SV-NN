9fca4f8109e6ec5ff70b1adb59d62cc894ffdf88
indexpro24@gmail.com <indexpro24@gmail.com>
2022-03-14 20:29:47 +0300

    git init

--- app/src/routes/index.svelte
+++ app/src/routes/index.svelte
@@ -146,11 +146,10 @@
   ```
 -->
 <div class="bg-white">
-	<div class="max-w-2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+	<div class="max-w-2xl mx-auto py-16 px-4 sm:py-16 sm:px-6 lg:max-w-7xl lg:px-8">
 		<h2 class="sr-only">Products</h2>
 
 		<div class="grid grid-cols-1 gap-y-4 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-
 
 			{#each seasonalGoods as {id, name, price, unit, img, link}}
 			<a sveltekit:prefetch href="/{link}" class="group">
@@ -161,7 +160,6 @@
 				<p class="mt-1 text-lg font-medium text-gray-900">{price} руб/{unit}</p>
 			</a>
 				{/each}
-
 
 		</div>
 	</div>
